8687ce71f6d99e0c55aa94a7ca203fc0fd182b80
Jeronimo Vargas <jeronimo@focus.uy>
2022-07-14 10:20:46 -0300

    Added settings for QuadSPI

--- src/QuadSPIAnalyzerSettings.h
+++ src/QuadSPIAnalyzerSettings.h
@@ -3,7 +3,6 @@
 
 #include <AnalyzerSettings.h>
 #include <AnalyzerTypes.h>
-
 class QuadSPIAnalyzerSettings : public AnalyzerSettings
 {
 public:
@@ -15,13 +14,47 @@ public:
 	virtual void LoadSettings( const char* settings );
 	virtual const char* SaveSettings();
 
-	
+	enum Mode{
+		QuadSPI,
+		QuadIO,
+		QPI
+	};
+
 	Channel mInputChannel;
 	U32 mBitRate;
+	Channel mClockChannel;
+	Channel mEnableChannel;
+	Channel mD0Channel;
+	Channel mD1Channel;
+	Channel mD2Channel;
+	Channel mD3Channel;
+	
+	BitState mEnableActiveState;
+	BitState mClockInactiveState;
+	AnalyzerEnums::Edge mDataValidEdge;
+	AnalyzerEnums::ShiftOrder mShiftOrder;
+	U32	mBytesPerTransfer;
+	U8 	mAddressByteLength;
+	/*QuadSpiEnums::*/Mode mQuadSpiMode;
 
 protected:
 	std::auto_ptr< AnalyzerSettingInterfaceChannel >	mInputChannelInterface;
-	std::auto_ptr< AnalyzerSettingInterfaceInteger >	mBitRateInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceInteger > 	mBitRateInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceChannel >	mClockChannelInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceChannel >	mEnableChannelInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceChannel >	mD0ChannelInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceChannel >	mD1ChannelInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceChannel >	mD2ChannelInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceChannel >	mD3ChannelInterface;
+
+	std::auto_ptr< AnalyzerSettingInterfaceNumberList >	mEnableActiveStateInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceNumberList >	mClockInactiveStateInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceNumberList >	mDataValidEdgeInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceNumberList >	mShiftOrderInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceNumberList >	mBytesPerTransferInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceNumberList >	mAddressByteLengthInterface; 
+	std::auto_ptr< AnalyzerSettingInterfaceNumberList >	mQuadSpiModeInterface; 
+
 };
 
 #endif //QUADSPI_ANALYZER_SETTINGS
